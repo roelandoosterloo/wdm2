5ebfb0d93d9238255d05f55830766a1eb940933a
Ka-Ping Wan <k.p.wan92@gmail.com>
2015-06-20 16:41:33 +0200

    df

--- src/Join.java
+++ src/Join.java
@@ -15,7 +15,7 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
         
 public class Join {
         
- public static class Map extends Mapper<LongWritable, Text, JoinWritable, RelationWritable> {
+ public static class Map extends Mapper<LongWritable, Text, IntWritable, RelationWritable> {
    
     private JoinWritable hashKey = new JoinWritable();
     private JoinWritable hashKeyRev = new JoinWritable();
@@ -23,99 +23,114 @@ public class Join {
     private final static int m = 3;
         
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        	String line = value.toString();
-    		
-    		String[] words =  line.split(" ");
-    		
-    		if(words.length >= 2){
-    			int a = Integer.parseInt(words[0]);
-        		int b = Integer.parseInt(words[1]);
-        		
-        		a = 1;
-        		b = 1;
-        		
-        		for(int i = 0 ; i <= m; i++){
-        			hashKey.set(a,b,i,m);
-            		hashKeyRev.set(b,a,i,m);
-            		relation.set(a,b,"A");
-            		context.write(hashKey, relation);
-            		context.write(hashKeyRev, relation);
-            		
-        			hashKey.set(a,i,b,m);
-            		hashKeyRev.set(b,i,a,m);
-            		relation.set(a,b,"B");
-            		context.write(hashKey, relation);
-            		context.write(hashKeyRev, relation);
-            		
-        			hashKey.set(i,a,b,m);
-            		hashKeyRev.set(i,b,a,m);
-            		relation.set(a,b,"C");
-            		context.write(hashKey, relation);
-            		context.write(hashKeyRev, relation);
-        		}
+        
+        
+            String line = value.toString();
+            
+            String[] words =  line.split(" ");
+            
+            if(words.length >= 2){
+                int a = Integer.parseInt(words[0]);
+                int b = Integer.parseInt(words[1]);
+                
+                for(int i = 0 ; i <= m; i++){
+                    hashKey.set(a,b,i,m);
+                    hashKeyRev.set(b,a,i,m);
+                    relation.set(a,b,"A");
+                    context.write(new IntWritable(hashKey.hashCode()), relation);
+                    context.write(new IntWritable(hashKeyRev.hashCode()), relation);
+                    
+                    hashKey.set(a,i,b,m);
+                    hashKeyRev.set(b,i,a,m);
+                    relation.set(a,b,"B");
+                    context.write(new IntWritable(hashKey.hashCode()), relation);
+                    context.write(new IntWritable(hashKeyRev.hashCode()), relation);
+                    
+                    hashKey.set(i,a,b,m);
+                    hashKeyRev.set(i,b,a,m);
+                    relation.set(a,b,"C");
+                    context.write(new IntWritable(hashKey.hashCode()), relation);
+                    context.write(new IntWritable(hashKeyRev.hashCode()), relation);
+                }
  
-    		}else{
-    			hashKey.set(5,5,5,5);
-        		relation.set(10,10,"B");
-       
-    			context.write(hashKey, relation);
-    		}
-    		
+            }
+            
     }
  } 
  
- public static class Reduce extends Reducer<JoinWritable, RelationWritable, JoinWritable, IntWritable> {
+ public static class Reduce extends Reducer<IntWritable, RelationWritable, IntWritable, IntWritable> {
 
- 	private IntWritable result = new IntWritable();
-	 
-    public void reduce(JoinWritable key, Iterable<RelationWritable> values, Context context) 
+    public List<RelationWritable> a_relation = new ArrayList<RelationWritable>();
+    public List<RelationWritable> b_relation = new ArrayList<RelationWritable>();
+    public List<RelationWritable> c_relation = new ArrayList<RelationWritable>();
+     
+    private IntWritable result = new IntWritable();
+    
+    public void reduce(IntWritable key, Iterable<RelationWritable> values, Context context) 
       throws IOException, InterruptedException {
-    	int count = 0;
-    	
-    	List<Edge> cacheA = new ArrayList<Edge>();
-    	List<Edge> cacheB = new ArrayList<Edge>();
-    	List<Edge> cacheC = new ArrayList<Edge>();
-    	
-    	for(RelationWritable value : values) {
-    		if(value.getRelation().equals("A")){ 
-    			cacheA.add(new Edge(value.getA(), value.getB()));
-    		} else if (value.getRelation().equals("B")) {
-    			cacheB.add(new Edge(value.getA(), value.getB()));
-    		} else {
-    			cacheC.add(new Edge(value.getA(), value.getB()));
-    		}
-    	}
-    	
-    	for(int i = 0; i < cacheA.size(); i++) {
-    		Edge e1 = cacheA.get(i);
-    		for(int j = 0; j < cacheB.size(); j++) {
-    			Edge e2 = cacheB.get(j);
-    			if(e1.getB() != e2.getA()){
-    				continue;
-    			}
-    			for(int k = 0; k < cacheC.size(); k++) {
-    				Edge e3 = cacheC.get(k);
-    				if(e2.getB() == e3.getA() && e3.getB() == e1.getA()) {
-    					count++;
-    					break;
-    				}
-    			}
-    		}
-    	}
-    	result.set(count);
-    	
-    	context.write(key, result);
-    	
+                
+        int count = 0;
+        
+        List<Edge> cacheA = new ArrayList<Edge>();
+        List<Edge> cacheB = new ArrayList<Edge>();
+        List<Edge> cacheC = new ArrayList<Edge>();
+        
+                
+        for(RelationWritable value : values) {
+            if(value.getRelation().equals("A")){ 
+                cacheA.add(new Edge(value.getA(), value.getB()));
+            } else if (value.getRelation().equals("B")) {
+                cacheB.add(new Edge(value.getA(), value.getB()));
+            } else {
+                cacheC.add(new Edge(value.getA(), value.getB()));
+            }
+        }
+        
+        
+
+        
+        /*for(Edge temp: cacheA){
+            context.write(key,new Text(temp.getA() + " " + temp.getB()));
+        }
+        
+        for(Edge temp: cacheB){
+            context.write(key,new Text(temp.getA() + " " + temp.getB()));
+        }
+        
+        for(Edge temp: cacheC){
+            context.write(key,new Text(temp.getA() + " " + temp.getB()));
+        }*/
+                
+        for(int i = 0; i < cacheA.size(); i++) {
+            Edge e1 = cacheA.get(i);
+            for(int j = 0; j < cacheB.size(); j++) {
+                Edge e2 = cacheB.get(j);
+                if(e1.getB() != e2.getA()){
+                    continue;
+                }
+                for(int k = 0; k < cacheC.size(); k++) {
+                    Edge e3 = cacheC.get(k);
+                    if(e2.getB() == e3.getA() && e3.getB() == e1.getA()) {
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+        result.set(count);
+        
+        context.write(key, result);
+        
     }
  }
         
  public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
         
-    Job job = new Job(conf, "inverted");
-
-    job.setMapOutputKeyClass(JoinWritable.class);
-    job.setMapOutputValueClass(RelationWritable.class);
+        Job job = Job.getInstance(conf);
+    
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(RelationWritable.class);
         
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(Text.class);
